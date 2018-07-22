7ac83301c2a07b14b39b53a6dde1e13882d8c519
webjazznl <hsiebelink73@gmail.com>
2018-07-22 20:37:51 +0200

    Disable linting rule for line

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import './index.css';
 import numeral from 'numeral';
 const courseValue = numeral(1000).format('$0,0.00');
 // add a breakpoint see devtool: 'inline-source-map' in webpack.config.dev.js
-debugger;
-console.log(`I would pay ${courseValue} for this course`);
+//debugger;
+console.log(`I would pay ${courseValue} for this course`); // eslint-disable-line no-console
