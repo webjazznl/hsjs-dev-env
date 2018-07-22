4c56777e94ee92724dae9005ba2bbc1e8c709d29
webjazznl <hsiebelink73@gmail.com>
2018-07-22 16:32:38 +0200

    Bundle our app with webpack\

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,14 +3,20 @@ import express from 'express';
 import path from 'path';
 // object top open a browser
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
 //define a port
 const port = 3000;
 // create an express application
 const app = express();
 //define action for '/'
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname,'../src/index.html'));
-});
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 //start listening on port and open a browser (log error if any)
 app.listen(port, function(error){
