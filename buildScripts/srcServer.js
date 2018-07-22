34732cfe1152df48c279c2b61616937740769d7d
webjazznl <hsiebelink73@gmail.com>
2018-07-22 08:10:03 +0200

    Add comments

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,17 +1,24 @@
+//use express as dev server
 var express = require('express');
+// support operations on request path
 var path = require('path');
+// object top open a browser
 var open = require('open');
 
+//define a port
 var port = 3000;
+// create an express application
 var app = express();
-
+//define action for '/'
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname,'../src/index.html'));
 });
+app.get('/')
 
-app.listen(port, function(err){
-  if(err) {
-    console.log(err);
+//start listening on port and open a browser (log error if any)
+app.listen(port, function(error){
+  if(error) {
+    console.log(error);
   } else {
     open('http://localhost:' + port);
   }
